b10d39bb48ed5bde4fb73fe93640f3afa1624d93
abdimk <abdisamk@gmail.com>
2023-04-24 13:26:28 +0300

    commit

--- Reference/main.cpp
+++ Reference/main.cpp
@@ -2,18 +2,7 @@
 
 using namespace std;
 
+int main(){
 
-enum Days: char
-{
-    A='M',B='T',C='W',D='T',E='F'
-};
-
-void Warn(const char* message){
-    cout<<message<<endl;
-}
-
-int main()
-{
-    Warn("Hello");
-    return 0;
+   
 }
